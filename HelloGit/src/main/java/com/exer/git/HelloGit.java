d6583685d588cdbd9578c82a1170f865cd511199
15080808941 <1789362147@qq.com>
2022-10-26 23:52:41 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/exer/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.exer.git;
+
+/**
+ * @author wangfei
+ * @create 2022-10-26 23:42
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
